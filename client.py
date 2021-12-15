c8c2507c8bf2c0b6d76cbae441f92b9d03e83526
kamighost1 <romires2000@gmail.com>
2021-12-15 08:47:51 +0300

    add flags

--- client.py
+++ client.py
@@ -1,17 +1,65 @@
+from logging import fatal
 from scapy.all import *
+import argparse
+import base64
 
-secret_tos = 0xD
-dest = '192.168.0.95'
-text = '\1hello my friend!\0'
+def formatMsg(data):
+    return "\1"+data+"\0"
 
-print(f"start sending to {dest}\nmsg: {text}")
+def formatMsgBase64(data):
+    return "\3"+data+"\0"
 
-i = 0
-for char in text:
+def formatMsgBase64File(data, name):
+    return "\3"+f"{name}\2"+ data +"\0"
+
+
+def sendStegano(mode, path):
+    secret_tos = 0xD
+    dest = '192.168.0.95'
+    text = '\1hello my friend!\0'
+    i = 0
     ip = IP(tos=secret_tos, dst=dest, flags='DF')
-    del ip.chksum
-    ip.chksum = 256 * i + ord(char)
-    send(ip)
-    i+=1
+    if(mode == False):
+        pass
+    if(mode == False and path!=False):
+        text = formatMsg(path)
+        pass
+    if(mode == True and path != False):
+        file = open(path, "rb")
+        text = file.read()
+        file.close()
+        text = base64.b64encode(text)
+        text = bytes.decode(text)
+        text = formatMsgBase64File(text, path)     
+        # print(text)   
+        # return
+    print(f"start sending to {dest}\nmsg: {text}")
+    for char in text:
+        # del ip.chksum
+        ip.chksum = ord(char)
+        send(ip, verbose=False)
+        i+=1
 
-print("done")
+    print("done")
+    pass
+
+
+def createParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument ('-f',type=str, help="path to file")
+    parser.add_argument ('-m',type=str, help="message")
+    return parser
+
+parser = createParser()
+namespace = parser.parse_args()
+
+if(namespace.f):
+    # print(namespace.f)
+    sendStegano(True, namespace.f)
+    pass
+elif (namespace.m):
+    sendStegano(False, namespace.m)
+    pass
+else:
+    sendStegano(False, False)
+
--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from scapy.all import *
+import time
+import base64
 
 msg = ""
+timeStart = 0
+timeStop = 0
+name = ""
+base = False
+isFile = False
 
 def sniffer():
     sniff(
@@ -11,15 +18,41 @@ def sniffer():
 
 def handler(x):
     global msg
+    global timeStart
+    global timeStop
+    global base
+    global isFile
+    global name
     symbol = chr(x[IP].chksum % 256)
-    if(symbol == '\1'):
+    if(symbol == '\1' or symbol == '\3'):
+        if(symbol == '\3'):
+            base = True
+            isFile = True
         msg = ""
         print("### new msg started ###")
-    if(symbol != '\0' ):
-        msg += symbol
-    else:
-        print(msg)
+        timeStart = time.time()
+    
+    elif(symbol == '\2'):
+        name = msg
         msg = ""
+        return
+    elif(symbol == '\0' ): 
+        timeStop = time.time()
+        if(timeStart > 0):
+            isTime = timeStop-timeStart
+            print(f"time is: {isTime}")
+        if(not base):
+            print(msg)
+        else:
+            msg = base64.b64decode(msg)
+            print(f"file {name} is load")
+            writer = open(name, "wb")
+            writer.write(msg)
+            writer.close()
+            pass
+        msg = ""
+    else:
+        msg += symbol
     pass
 
 print("start sniffing")
